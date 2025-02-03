cbe061b6870e86dcbd342ac79ac9aea92a97fde8
suzy <suzyp0223@gmail.com>
2025-02-03 20:13:09 +0900

    init commit

---
+++ src/react-app.env.d.ts
@@ -0,0 +1,21 @@
+// SVG 외에도 이미지 파일을 인식해야 하는 경우
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+declare module '*.svg' {
+  const content: string;
+  export default content;
+}
+
+// CSS, SCSS, 모듈 CSS를 인식해야 하는 경우
+declare module '*.css';
+declare module '*.scss';
+declare module '*.module.css' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+declare module '*.module.scss' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
